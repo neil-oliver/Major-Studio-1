c9fdd7c676d7e3ec1ec838bc923c9292f122c187
neil-oliver <neiloliverphoto@gmail.com>
2019-11-09 15:46:16 -0500

    basic working version

--- Interactivity/index.js
+++ Interactivity/index.js
@@ -17,6 +17,10 @@ var svg = d3.select("#content")
 .append("g")
 .attr("transform","translate(" + margin.left + "," + margin.top + ")")
 
+
+////////////////////////////////////
+
+
 async function draw(data, metObjects) {
   var spacing = 200
   
@@ -63,7 +67,7 @@ async function draw(data, metObjects) {
     })
     .attr('class', 'years');
 
-    d3.select('svg g')
+  d3.select('svg g')
     .selectAll('.titles')
     .data(data.nodes)
     .join('text')
@@ -71,35 +75,6 @@ async function draw(data, metObjects) {
     .attr('y', (d,i) => spacing*(i+1))
     .text((d) => metObjects[d.id.split('-')[1]].title)
     .attr('class', 'titles');
-
-
-  // Add links between nodes. Here is the tricky part.
-  // In my input data, links are provided between nodes -id-, NOT between node names.
-  // So I have to do a link between this id and the name
-  var idToNode = {};
-  data.nodes.forEach(function (n) {
-    idToNode[n.id] = n;
-  });
-  // Cool, now if I do idToNode["2"].name I've got the name of the node with id 2
-
-  // Add the links
-  svg
-    .selectAll('mylinks')
-    .data(data.links)
-    .enter()
-    .append('path')
-    .attr('d', function (d) {
-      start = x(idToNode[d.source].name)    // X position of start node on the X axis
-      end = x(idToNode[d.target].name)      // X position of end node
-      return ['M', start, height-30,    // the arc starts at the coordinate x=start, y=height-30 (where the starting node is)
-        'A',                            // This means we're gonna build an elliptical arc
-        (start - end)/2, ',',    // Next 2 lines are the coordinates of the inflexion point. Height of this point is proportional with start - end distance
-        (start - end)/2, 0, 0, ',',
-        start < end ? 1 : 0, end, ',', height-30] // We always want the arc on top. So if end is before start, putting 0 here turn the arc upside down.
-        .join(' ');
-    })
-    .style("fill", "none")
-    .attr("stroke", "black")
 
   return Promise.resolve()
 }
